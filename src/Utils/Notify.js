519942208df6f0057541717ede81a3d9cd3b907e
MOSTAFA <mhrmo75@gmail.com>
2025-07-29 23:29:30 +0330

    notify

---
+++ src/Utils/Notify.js
@@ -0,0 +1,9 @@
+import toast from "react-hot-toast";
+
+const Notify=(type,message)=>{
+    toast[type](message,{
+     duration: 4000,
+    position: "bottom-center",
+    })
+}
+export default Notify
